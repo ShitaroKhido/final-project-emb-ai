f06780834125322543f500c256bc952414173e8b
Khid <khid@niy.ai>
2025-05-29 11:55:53 +0700

    [update]

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+from EmotionDetector.emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Welcome to the Flask App!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
